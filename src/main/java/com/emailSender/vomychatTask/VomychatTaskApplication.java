db556e51ce87dcfd4281e5b2578bd84ef579ec8d
khilola.kushbakova <xilola_kushbakova@mai.ru>
2025-01-26 12:18:01 +0100

    Initial commit

---
+++ src/main/java/com/emailSender/vomychatTask/VomychatTaskApplication.java
@@ -0,0 +1,13 @@
+package com.emailSender.vomychatTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VomychatTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VomychatTaskApplication.class, args);
+	}
+
+}
